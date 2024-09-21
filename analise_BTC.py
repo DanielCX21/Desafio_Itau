64451cd80545f9d0a88e861a77b429a18f10fd82
DanielCX21 <daniel.almeida.castro@hotmail.com>
2024-09-21 13:06:52 -0300

    atualizacoes

--- analise_BTC.py
+++ analise_BTC.py
@@ -7,38 +7,17 @@ import matplotlib.pyplot as plt
 with open('dados/dados-BTC.json', 'r') as arquivo:
     dados = json.load(arquivo)
 
-#Acessar a lista de dados no campo 'Data' do JSON
-#Forma esquisita de armazenar, porem lista_dados
-#é a lista de interesse que contém os valores que precisamos
 lista_dados = dados['Data']['Data']
-#vamos criar um vetor com as datas
-#preciso somente desse arquivo para 
-#me dar as informações nescessarias
-
 #para criar algo sobre o momento da curva, vamos interpolar
 #com os pontos pontos (x,y) conforme a curva for "se revelando"
 #na troca de concavidade possivelmente será um ponto a ser 
 #estudado
-
 medo_BTC = list()
 volume_to_BTC= list()
 volume_from_BTC = list()
 preco_open_BTC = list()
 preco_close_BTC = list()
 data_BTC = list()
-media = 0
-
-#for valor in lista_dados:
-#    media += valor['fear_greed_value']
-
-#media = media
-#media = media / len(lista_dados)
-#media = int(media)
-
-#for valor in lista_dados:
-#    medo_BTC.append(valor['fear_greed_value'] - media)
-
-#ou...
 
 for valor in lista_dados:
     medo_BTC.append(valor['fear_greed_value'] - 50)
@@ -59,30 +38,3 @@ for valor in lista_dados:
     data = transform_data.unix_dh(valor['time'])
     data = data[:-9] 
     data_BTC.append(data)
-
-der_seg_preco = list()
-
-for i in range(1, len(medo_BTC) - 1):
-    x = np.array([i - 1,i,i + 1])
-    y = np.array([preco_close_BTC[i - 1],
-                  preco_close_BTC[i],
-                  preco_close_BTC[i + 1]])
-    interpol = lagrange(x, y)
-    interpol = interpol.deriv()
-    #interpol = interpol.deriv()
-    der_seg_preco.append(int(interpol(i)))
-#tenho uma lista com todas as segundas derivadas do preco
-
-eixo_x = list()
-
-for i in range(len(medo_BTC) - 2):
-    eixo_x.append(i)
-
-'''
-plt.plot(eixo_x,der_seg_preco,color='blue')
-plt.plot(data_BTC,medo_BTC,color='red')
-primeira_data = data_BTC[0]
-ultima_data = data_BTC[-1]
-plt.gca().set_xticks([primeira_data, ultima_data])
-plt.show() 
-'''
--- rascunho.py
+++ rascunho.py
@@ -1,42 +1,27 @@
 import numpy as np
-import matplotlib.pyplot as plt
 from scipy.interpolate import CubicSpline
 from analise_BTC import medo_BTC
-from scipy.optimize import fsolve
 
 x_pontos = np.arange(1,2002,dtype=int)
 y_pontos = np.array(medo_BTC)
 tamanho = len(medo_BTC)
-
 #print(len(medo_BTC)) = 2001
 
 cs_ex = CubicSpline(x_pontos,y_pontos)
-
 x_suave = np.linspace(x_pontos[0],x_pontos[-1], tamanho * 100)
-y_suave = cs_ex(x_suave)
-
 cs_deriv = cs_ex.derivative()
 cs_seg_deriv = cs_deriv.derivative()
-
-y_deriv = cs_deriv(x_suave)
 y_seg_deriv = cs_seg_deriv(x_suave)
 
-pontos_criticos_x = fsolve(cs_deriv, x_suave)
-pontos_criticos_y = cs_ex(pontos_criticos_x)
+coeficientes_seg_deriv = cs_seg_deriv.c
 
-pontos_inflexao_x = fsolve(cs_seg_deriv, x_suave)
-pontos_inflexao_y = cs_ex(pontos_inflexao_x)
+pontos_inflexao_x = list()
 
-#plt.subplot(1, 2, 1)
-plt.plot(x_pontos, y_pontos, 'o', color='red', label='Pontos Originais')
-plt.plot(x_suave, y_suave, color='blue', label='Cubic Spline')
-plt.scatter(pontos_criticos_x, pontos_criticos_y, color='orange')
-#plt.subplot(1, 2, 2)
-plt.plot(x_suave, y_deriv, color='green', label='Derivada da Spline')
-plt.scatter(pontos_inflexao_x, pontos_inflexao_y, color='purple')
-plt.grid()
-plt.tight_layout()
-plt.show()
-
-for i in range(len(x_pontos) - 1):
-    print(f"Coefs {i}: {cs_ex.c[:,i]}")
+#y = b + a(x - (i + 1))
+#y = (b - a) + ax
+#x = (a - b) / a
+for i in range(coeficientes_seg_deriv.shape[1] - 1950):
+    print(f"Intervalo [{i},{i + 1}]")
+    print(f"Coeficiente de x: {coeficientes_seg_deriv[0, i]}") #a
+    print(f"Coeficiente independente: {coeficientes_seg_deriv[1, i]}") #b  
+    print(y_seg_deriv[i * 100])
