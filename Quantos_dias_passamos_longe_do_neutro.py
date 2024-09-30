2ef6d0609f69a42969b78fa91aa3ee230512ba4a
DanielCX21 <daniel.almeida.castro@hotmail.com>
2024-09-30 14:30:57 -0300

    quantidade medo

--- Quantos_dias_passamos_longe_do_neutro.py
+++ Quantos_dias_passamos_longe_do_neutro.py
@@ -1,21 +1,28 @@
 from analise_BTC import medo_BTC
 from math import fabs
 
-quantidade = 0
+quantidade = len(medo_BTC)
 ganancia = 0
 medos = 0
 neutro = 0
+extremo_medo = 0
+extrema_ganancia = 0
 
 for medo in medo_BTC:
-    if fabs(medo) > 5:
-        quantidade += 1
-        if medo > 0:
+    if medo > 0:
+        if medo > 20:
+            extrema_ganancia += 1
+        if medo > 5:
             ganancia += 1
-        else:
+    if medo < 0:
+        if medo < -25:
+            extremo_medo += 1
+        if medo < -5:
             medos += 1
-    else:
-        if medo == 0:
-            neutro += 1
+    if medo == 0:
+        neutro += 1
 
 print("Passamos {} dias, cerca de {:.2f} % do tempo fora da neutralidade, sendo {} dias na ganancia e {} dias no medo".format(quantidade,(quantidade/2001)*100,ganancia,medos))
 print("Passamos {} dias em neutro: (50) -> {:.2f}% do tempo!".format(neutro, ((neutro/len(medo_BTC)*100))))
+print("Passamos {} dias em extremo medo".format(extremo_medo))
+print("Passamos {} dias em extrema ganancia".format(extrema_ganancia))
